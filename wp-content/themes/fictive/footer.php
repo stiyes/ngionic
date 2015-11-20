d35b8a46974a1ff95862ccc4c0f3169320b5fd71
phil <stiyes@yeah.net>
2015-11-20 16:02:33 +0800

    fixed

--- wp-content/themes/fictive/footer.php
+++ wp-content/themes/fictive/footer.php
@@ -12,14 +12,29 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'fictive' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'fictive' ), 'WordPress' ); ?></a>
+			<a href="/">首页</a>	
 			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'fictive' ), 'Fictive', '<a href="https://wordpress.com/themes/" rel="designer">WordPress.com</a>' ); ?>
+			<a href="http://ngionic.com/30-minutes-learn-angular/">Angularjs入门</a>	
+			<span class="sep"> | </span>
+			<a href="http://ngionic.com/step-start-ionic/">Ionic入门</a>
+<span class="sep"> | </span>
+			<a href="http://ngionic.com/api/ionic/v1.0.0-beta.13/components/css" title="Ionic中文API-css篇">Ionic中文API-css篇</a>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
+
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?76bbdcf0d8e1eabea37d17503a7df7ee";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
+
 
 </body>
 </html>
